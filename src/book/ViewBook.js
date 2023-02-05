b8fd909a1f3a6be32029de14424b27ae8a11bf87
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-05 18:36:33 +0530

    create view book page

--- src/book/ViewBook.js
+++ src/book/ViewBook.js
@@ -1,7 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Card from "../components/Card";
 
 const ViewBook = () => {
-  return <div>ViewBook</div>;
+  const [book, setBook] = useState({
+    id: "",
+    name: "",
+    author: "",
+    readStatus: "",
+  });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    loadData(id);
+  }, []);
+
+  const loadData = async (id) => {
+    const result = await axios.get(`http://localhost:8080/api/books/${id}`);
+    setBook(result.data);
+  };
+
+  return book && <Card book={book} />;
 };
 
 export default ViewBook;
