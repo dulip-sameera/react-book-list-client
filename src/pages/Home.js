946b79c3eb61845a91f35d78bedff6edb07a8fc6
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-05 18:18:10 +0530

    create home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,85 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const result = await axios.get("http://localhost:8080/api/books");
+    setBooks(result.data);
+  };
+
+  const deleteBook = async (id) => {
+    await axios.delete(`http://localhost:8080/api/books/${id}`);
+    loadData();
+  };
+
+  return (
+    <div className="container">
+      <div className="my-4">
+        <table className="table shadow text-light">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Author</th>
+              <th scope="col">Number of Pages</th>
+              <th scope="col">Read Status</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* incase loaddata function took time to populate the state */}
+            {books &&
+              books.map((book, index) => {
+                return (
+                  <tr key={index + 1}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{book.name}</td>
+                    <td>{book.author}</td>
+                    <td>{book.noOfPages}</td>
+                    <td>
+                      {book.readStatus.name === "true" ? (
+                        <span className="text-success">Yes</span>
+                      ) : (
+                        <span className="text-danger">No</span>
+                      )}
+                    </td>
+                    <td>
+                      <div>
+                        <Link
+                          to={"/view-book"}
+                          className="btn btn-primary mx-2"
+                        >
+                          View
+                        </Link>
+                        <Link
+                          to={"/edit-book"}
+                          className="btn btn-outline-light mx-2"
+                        >
+                          Edit
+                        </Link>
+                        <button
+                          className="btn btn-danger mx-2"
+                          onClick={() => deleteBook(book.id)}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
